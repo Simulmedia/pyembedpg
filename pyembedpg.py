e2f45599f70003361dbd0c78b63d01d73a3aa113
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2015-07-28 11:30:10 -0400

    formatting

--- pyembedpg.py
+++ pyembedpg.py
@@ -1,3 +1,4 @@
+#
 # Copyright 2015 Simulmedia, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#
 # Copyright 2015 Simulmedia, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
